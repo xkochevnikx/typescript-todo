a7c5a8e35ca58cc3340adfdbd681b9da90519e85
xkochevnikx <xkochevnikx@protonmail.com>
2023-11-02 10:20:35 +0300

    add types for state

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { useState } from 'react';
 import { TodoItem } from './components/TodoItem';
-
+type TTodo = {
+    id: string;
+    title: string;
+    completed: boolean;
+};
 const App = () => {
+    const [text, setText] = useState('');
+
+    const [todos, setTodos] = useState<string[]>([]);
+
+    const [] = useState<TTodo | null>(null);
+
     return (
         <div className="App">
             <TodoItem
